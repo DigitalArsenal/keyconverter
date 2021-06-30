5148ac93f06a15c34bf4b293f197c1eb8ab26ef4
tj <tj@digitalarsenal.io>
2021-06-29 21:07:50 -0400

    update API

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,37 @@
 import base64URL from "base64url";
+import { crypto as linerCrypto } from "webcrypto-liner/build/index.js";
+import * as bitcoinjs from 'bitcoinjs-lib';
+import wif from "wif";
+import { pbkdf2Sync } from 'crypto';
+import * as x509 from '@peculiar/x509';
+import { writeFileSync } from 'fs';
+import inquirer from 'inquirer';
+import { of } from "rxjs";
+import sshpk from 'sshpk';
+
+let { subtle } = linerCrypto;
 
 export class keymaster {
 
     privateKey: CryptoKey;
     publicKey: CryptoKey;
     curve: NamedCurve;
-    jwk: JsonWebKey;
 
-    private static jwkConversion(prvHex, namedCurve): JsonWebKey {
+
+    /*
+    encrypt: The key may be used to encrypt messages.
+    decrypt: The key may be used to decrypt messages.
+    sign: The key may be used to sign messages.
+    verify: The key may be used to verify signatures.
+    deriveKey: The key may be used in deriving a new key.
+    deriveBits: The key may be used in deriving bits.
+    wrapKey: The key may be used to wrap a key.
+    unwrapKey: The key may be used to unwrap a key.
+    */
+
+    keyUsages: Array<string> = ["sign", "verify"];
+
+    private static jwkConversion(prvHex: string, namedCurve: NamedCurve): JsonWebKey {
         return {
             kty: "EC",
             crv: namedCurve,
@@ -20,14 +44,22 @@ export class keymaster {
     get bip39(): string {
         return ""
     }
-    constructor(curve: string, privateKey: Buffer);
-    constructor(curve: string, privateKey: string, format: string);
-    constructor(curve: string, bip39: string);
-    constructor(curve: string, ...args: any[]) {
-        if (args[0] instanceof Buffer) {
-            let _jwk: JsonWebKey = keymaster.jwkConversion(args[0].toString('hex'), curve);
-            console.log(_jwk);
-        }
+
+    init(privateKey: Buffer);
+    init(bip39: string);
+    init(privateKey: string, format: string);
+
+    public async init(privateKey: any, format?: string): Promise<void> {
+        this.privateKey = await subtle.importKey(
+            "jwk",
+            keymaster.jwkConversion(privateKey.toString('hex'), this.curve),
+            { name: "ECDSA", namedCurve: this.curve },
+            true,
+            this.keyUsages);
+    }
+
+    constructor(namedCurve: NamedCurve) {
+        this.curve = namedCurve;
     }
 }
 
--- test/certificate.creation.ts
+++ test/certificate.creation.ts
@@ -3,8 +3,13 @@ import { keymaster } from "../src/index";
 import { pbkdf2Sync, scryptSync } from 'crypto';
 import * as argon2 from "argon2";
 
-console.log(argon2);
+let curves = {
+    secp256k1: 'K-256',
+    secp256r1: 'P-256',
+}
 
-it("creates a keymaster instance", async function () {
-    let km = new keymaster('P-256', pbkdf2Sync('Test', 'Test', 1, 32, "sha256"));
+it("creates a certificate from a buffer", async function () {
+    let km = new keymaster('K-256');
+    await km.init(pbkdf2Sync('Test', 'Test', 1, 32, "sha256"));
+    console.log(km);
 });
