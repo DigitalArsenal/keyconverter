8879d528373bb6e2877817cbdee2e46cda6595b2
tjkoury <tjkoury@gmail.com>
2021-07-03 22:53:08 -0400

    test working

--- test/certificate.creation.ts
+++ test/certificate.creation.ts
@@ -6,14 +6,16 @@ import * as argon2 from "argon2";
 let curves = {
     secp256k1: 'K-256',
     secp256r1: 'P-256',
-}
+};
 
 it("creates a certificate from a buffer", async function () {
     let km = new keymaster(curves.secp256k1);
-    let privateKeyHex = new Array(64).join("0")+"1";
-    await km.init(Buffer.from(
+    let privateKeyHex = new Array(64).join("0") + "1";
+    let privateKeyHexBuffer = Buffer.from(
         privateKeyHex,
-        'hex'));
+        'hex');
+    let bip39 = `abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon diesel`;
+    await km.init(privateKeyHexBuffer);
     assert.strictEqual((await km.hex()), privateKeyHex);
-    assert.strictEqual(await km.bip39(), `abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon diesel`);
+    assert.strictEqual(await km.bip39(), bip39);
 });
