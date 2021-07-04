62e34d0ddbff2110526f61657d10a9705882d347
tjkoury <tjkoury@gmail.com>
2021-07-03 21:11:07 -0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,23 @@
-import base64URL from "base64url";
-import { crypto as linerCrypto } from "webcrypto-liner/build/index.js";
-import * as bitcoinjs from 'bitcoinjs-lib';
-import wif from "wif";
-import { pbkdf2Sync } from 'crypto';
-import * as x509 from '@peculiar/x509';
-import { writeFileSync } from 'fs';
-import inquirer from 'inquirer';
-import { of } from "rxjs";
-import sshpk from 'sshpk';
-import * as bip39 from 'bip39';
+import base64URL from 'base64url'
+import { crypto as linerCrypto } from 'webcrypto-liner/build/index.js'
+import * as bitcoinjs from 'bitcoinjs-lib'
+import wif from 'wif'
+import { pbkdf2Sync } from 'crypto'
+import * as x509 from '@peculiar/x509'
+import { of } from 'rxjs'
+import sshpk from 'sshpk'
+import * as bip39 from 'bip39'
 
+let { subtle } = linerCrypto
 
-let { subtle } = linerCrypto;
-
-type HexKey = string;
+type EncodingOptions = BufferEncoding | 'wif';
 
 export class keymaster {
+  privateKey: CryptoKey
+  publicKey: CryptoKey
+  curve: NamedCurve
 
-    privateKey: CryptoKey;
-    publicKey: CryptoKey;
-    curve: NamedCurve;
-
-    /*
+  /*
     encrypt: The key may be used to encrypt messages.
     decrypt: The key may be used to decrypt messages.
     sign: The key may be used to sign messages.
@@ -32,58 +28,60 @@ export class keymaster {
     unwrapKey: The key may be used to unwrap a key.
     */
 
-    keyUsages: Array<string>;
+  keyUsages: Array<string>
 
-    /**
-     * Converts hex format to an RFC7517 JSONWebKey
-     * @link https://datatracker.ietf.org/doc/html/rfc7517
-     * @param 
-     * @returns {Number} Returns the value of x for the equation.
-     */
-    private static jwkConversion(prvHex: string, namedCurve: NamedCurve, format: string = "hex"): JsonWebKey {
-        return {
-            kty: "EC",
-            crv: namedCurve,
-            d: base64URL.encode(prvHex, format),
-            x: null,
-            y: null,
-        }
-    };
-
-    get bip39(): string {
-        return ""
+  /**
+   * Converts hex format to an RFC7517 JSONWebKey
+   * @link https://datatracker.ietf.org/doc/html/rfc7517
+   * @param
+   * @returns {Number} Returns the value of x for the equation.
+   */
+  private static jwkConversion (
+    prvHex: string,
+    namedCurve: NamedCurve,
+    format: string = 'hex'
+  ): JsonWebKey {
+    return {
+      kty: 'EC',
+      crv: namedCurve,
+      d: base64URL.encode(prvHex, format),
+      x: null,
+      y: null
     }
-    init(privateKey: Buffer);
-    init(privateKey: string, format?: BufferEncoding);
+  }
 
-    public async init(privateKey: any, format?: BufferEncoding): Promise<void> {
+  get bip39 (): string {
+    return ''
+  }
+  init (privateKey: Buffer)
+  init (privateKey: string, format?: EncodingOptions)
 
-        let convert: Boolean = false;
+  public async init (privateKey: any, format?: EncodingOptions): Promise<void> {
+    let convert: Boolean = false
 
-        if (typeof privateKey === "string" && privateKey.indexOf(' ') > -1) {
-            privateKey = bip39.mnemonicToEntropy(privateKey);
-            convert = true;
-
-        } else if (privateKey instanceof Buffer) {
-            convert = true;
-        }
-
-        if (convert) {
-            format = "hex";
-            privateKey = privateKey.toString("hex");
-        }
-
-        this.privateKey = await subtle.importKey(
-            "jwk",
-            keymaster.jwkConversion(privateKey, this.curve, format),
-            { name: "ECDSA", namedCurve: this.curve },
-            true,
-            this.keyUsages);
+    if (typeof privateKey === 'string' && privateKey.indexOf(' ') > -1) {
+      privateKey = bip39.mnemonicToEntropy(privateKey)
+      convert = true
+    } else if (privateKey instanceof Buffer) {
+      convert = true
     }
 
-    constructor(namedCurve: NamedCurve, keyUsages?: Array<string>) {
-        this.curve = namedCurve;
-        this.keyUsages = keyUsages || ["sign", "verify"];
+    if (convert) {
+      format = 'hex'
+      privateKey = privateKey.toString('hex')
     }
+
+    this.privateKey = await subtle.importKey(
+      'jwk',
+      keymaster.jwkConversion(privateKey, this.curve, format),
+      { name: 'ECDSA', namedCurve: this.curve },
+      true,
+      this.keyUsages
+    )
+  }
+
+  constructor (namedCurve: NamedCurve, keyUsages?: Array<string>) {
+    this.curve = namedCurve
+    this.keyUsages = keyUsages || ['sign', 'verify']
+  }
 }
-
