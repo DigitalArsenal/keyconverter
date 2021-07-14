b5c7d151b98177ff4e27d0c8b5dccec919240b1f
tj <tj@digitalarsenal.io>
2021-07-13 23:16:23 -0400

    rollup working

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from "@rollup/plugin-node-resolve";
+
+export default {
+    input: 'src/keyconvert.ts',
+    output: {
+        dir: 'build',
+        format: 'esm',
+    },
+    plugins: [typescript(), commonjs(), json(), resolve()],
+};
