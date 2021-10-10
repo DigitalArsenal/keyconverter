c033bac0dc4a444559f33ff87923a3c8f0c2b54e
tjkoury <tj@digitalarsenal.io>
2021-10-10 16:59:48 -0400

    updated with node shims

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 export default {
-    input: 'src/keyconvert.ts',
-    output: {
-        dir: 'build',
-        format: 'esm',
-    },
-    plugins: [typescript({
-        "module": "esnext",
-    }), commonjs(), json(), resolve({ preferBuiltins: true })],
+  input: "src/keyconvert.ts",
+  output: {
+    dir: "build",
+    format: "esm"
+  },
+  plugins: [
+    nodePolyfills({ include: ["buffer", "crypto", "stream", "fs", "path", "os"] }),
+    typescript({
+      module: "esnext"
+    }),
+    commonjs(),
+    json(),
+    resolve({ preferBuiltins: true })
+  ]
 };
